936043ce8f99817e4111035a25af57156bda9a05
Maxigong <maxi@nolgong.com>
2022-06-09 09:34:03 +0900

    favicon

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
       <Title> Nuxt Assistant App </Title>
       <Meta name="description" content="Nuxt Pinia TODO app" />
     </Head>
-    <Link rel="preload" href="./assets/favicon.ico" />
+    <Link rel="preload" href="./favicon.ico" />
   </Html>
   <div>
     <Navbar />
