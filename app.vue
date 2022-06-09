5838fa222dd09431a40e40ec4a445a096933e0ad
Maxi Ruti Park <84664090+maxrpark@users.noreply.github.com>
2022-06-09 12:04:19 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
       <Title> Nuxt Assistant App </Title>
       <Meta name="description" content="Nuxt Pinia TODO app" />
     </Head>
-    <Link rel="icon" type="image/x-icon" href="./favicon.ico" />
+    <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
   </Html>
   <div>
     <Navbar />
